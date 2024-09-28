25a4fbe0049052a3d0baa863c5906aa25904d828
acconisprogrammer <wedsdsa22@gmail.com>
2024-09-28 20:07:27 +0500

    created project

---
+++ src/main/java/com/pluralsight/taskmaster/controller/AuthController.java
@@ -0,0 +1,34 @@
+package com.pluralsight.taskmaster.controller;
+
+import com.pluralsight.taskmaster.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService service;
+
+
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(service.register(request));
+    }
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(
+            @RequestBody AuthenticationRequest request
+    ) {
+        return ResponseEntity.ok(service.login(request));
+    }
+}
